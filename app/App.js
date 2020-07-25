946c59a705fb1d54a9ed6157420a7942d31ff1af
Adi Kishore <kishek12@gmail.com>
2020-07-25 10:49:31 +1000

    pushing our work up from tute meeting

--- app/App.js
+++ app/App.js
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, Image } from "react-native";
+import { MAIN_PRIMARY_COLOUR, ScreenHeadingStyles } from "./constants";
+import { AppLoading } from "expo";
+import {
+  useFonts,
+  WorkSans_300Light,
+  WorkSans_400Regular,
+  WorkSans_500Medium,
+  WorkSans_700Bold,
+  WorkSans_800ExtraBold,
+} from "@expo-google-fonts/work-sans";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    WorkSans_300Light,
+    WorkSans_400Regular,
+    WorkSans_500Medium,
+    WorkSans_700Bold,
+    WorkSans_800ExtraBold,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return <HomeScreen />;
+  }
+}
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#EEEEEE",
+        alignItems: "center",
+      }}
+    >
+      <Text style={ScreenHeadingStyles}>Saved Trips</Text>
+      <Text
+        style={{
+          fontSize: 18,
+          color: MAIN_PRIMARY_COLOUR,
+          fontFamily: "WorkSans_400Regular",
+          marginTop: 8,
+        }}
+      >
+        View your saved trips and their details.
+      </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function SplashScreen() {
+  return (
+    <View
+      style={{
+        backgroundColor: MAIN_PRIMARY_COLOUR,
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View>
+        <Image
+          source={require("./assets/logo.png")}
+          style={{ alignSelf: "center", width: 33.6, height: 42 }}
+        />
+        <Text
+          style={{
+            fontSize: 42,
+            fontFamily: "WorkSans_800ExtraBold",
+            textAlign: "center",
+            color: "white",
+            marginBottom: 14,
+          }}
+        >
+          transport.me
+        </Text>
+        <Text
+          style={{
+            fontSize: 12,
+            textAlign: "center",
+            color: "white",
+            fontFamily: "WorkSans_400Regular",
+          }}
+        >
+          Manage your complete transport NSW experience.
+        </Text>
+      </View>
+    </View>
+  );
+}
---
+++ app/constants/index.js
@@ -0,0 +1,7 @@
+export const MAIN_PRIMARY_COLOUR = "#3A3957";
+export const ScreenHeadingStyles = {
+  fontSize: 54,
+  fontFamily: "WorkSans_500Medium",
+  color: MAIN_PRIMARY_COLOUR,
+  marginTop: 84,
+};
