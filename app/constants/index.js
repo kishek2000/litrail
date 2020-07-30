c724f51e42a72b77b1d251eb691760efeade16a8
Adi Kishore <kishek12@gmail.com>
2020-07-30 19:13:16 +1000

    sub-team 2: Daniel and Adi committing in multi select for edit trips. TODO: deletion method

--- app/constants/index.js
+++ app/constants/index.js
@@ -62,6 +62,8 @@ export const MenuBar = [
   },
 ];
 
+export const UserTrips = [];
+
 export const DefinedTrips = [
   {
     startStop: "Bella Vista Station",
--- app/screens/RoutesScreen.js
+++ app/screens/RoutesScreen.js
@@ -2,6 +2,14 @@ import React from "react";
 import { View, SafeAreaView, Text } from "react-native";
 import { ScreenHeadingStyles, MAIN_PRIMARY_COLOUR } from "../constants";
 
+// Within each screen, you can have a stack of screens
+// Creating a stack nav, react navigation docs
+// 4 screens in stack:
+//  -> initial screen => saved trip card component (same as homescreen)
+//  -> times => trip time component (similar to above component)
+//  -> details for a trip => trip detail component
+//  -> expand details for a trip => expand view component (similar to above component)
+
 export function RoutesScreen({ navigation }) {
   return (
     <SafeAreaView style={{ flex: 1 }}>
