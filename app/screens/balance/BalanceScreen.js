532faea90175f81bf175024eebab1ba3c2d9b9df
Daniel <danielkang252@gmail.com>
2020-08-08 13:38:59 +1000

    completed the input text boxes for opalCardNum and Password

--- app/screens/balance/BalanceScreen.js
+++ app/screens/balance/BalanceScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, SafeAreaView, Text } from "react-native";
+import { View, SafeAreaView, Text, TextInput } from "react-native";
 import { MAIN_PRIMARY_COLOUR, ScreenHeadingStyles } from "../../constants";
 import { useNavigation } from "@react-navigation/native";
 
@@ -14,6 +14,8 @@ export function BalanceScreen({ currentUser }) {
           backgroundColor: "#EEEEEE",
           alignItems: "center",
           position: "relative",
+          paddingHorizontal: 16,
+
         }}
       >
         <Text style={ScreenHeadingStyles}>Balance</Text>
@@ -27,7 +29,32 @@ export function BalanceScreen({ currentUser }) {
         >
           View and manage your Opal Balance.
         </Text>
+      <Text style={{
+        fontFamily: "WorkSans_700Bold",
+        marginTop: 24,
+        textAlign: "left",
+        alignSelf: "flex-start",
+      }}>OPAL BALANCE</Text>
+      <OpalCardInput placeholder = "Opal Card Number..."/>
+      <OpalCardInput placeholder = "Opal Security Code or Password..."/>
       </View>
     </SafeAreaView>
   );
+}
+
+function OpalCardInput({placeholder}) {
+  return (
+  <TextInput style= {{
+    height: 46,
+    backgroundColor: "white",
+    width: "100%",
+    borderRadius: 24,
+    paddingLeft: 24,
+    fontFamily: "WorkSans_300Light",
+    fontSize: 16,
+    color: MAIN_PRIMARY_COLOUR,
+    marginTop: 8,
+  }}
+  placeholder = {placeholder}
+  />)
 }
