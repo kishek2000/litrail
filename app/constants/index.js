af2092769a52c1478847537a6e21d724b9ba6430
Daniel <danielkang252@gmail.com>
2020-08-09 11:49:50 +1000

    Added states for the balance screen and updated the index.js

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,36 +1,35 @@
-import React from "react";
+import React from 'react';
 
-import busIcon from "../assets/bus.png";
+import busIcon from '../assets/bus.png';
 
 export const busIconIcon = busIcon; // temporary
 
-import walkIcon from "../assets/walk.png";
+import walkIcon from '../assets/walk.png';
 export const walkIconIcon = walkIcon; // temporary
 
-import seatAvailabilityImage from "../assets/seats.png";
+import seatAvailabilityImage from '../assets/seats.png';
 export const seatAvailabilityIcon = seatAvailabilityImage;
 
+import lightrailIcon from '../assets/lightrail.png';
+import trainIcon from '../assets/train.png';
 
-import lightrailIcon from "../assets/lightrail.png";
-import trainIcon from "../assets/train.png";
+import homeIcon from '../assets/home.png';
+import routesIcon from '../assets/routes.png';
+import schedulesIcon from '../assets/schedules.png';
+import balanceIcon from '../assets/balance.png';
+import homeIconFocused from '../assets/home-focused.png';
+import routesIconFocused from '../assets/routes-focused.png';
+import schedulesIconFocused from '../assets/schedules-focused.png';
+import balanceIconFocused from '../assets/balance-focused.png';
 
-import homeIcon from "../assets/home.png";
-import routesIcon from "../assets/routes.png";
-import schedulesIcon from "../assets/schedules.png";
-import balanceIcon from "../assets/balance.png";
-import homeIconFocused from "../assets/home-focused.png";
-import routesIconFocused from "../assets/routes-focused.png";
-import schedulesIconFocused from "../assets/schedules-focused.png";
-import balanceIconFocused from "../assets/balance-focused.png";
+import editImage from '../assets/edit.png';
+import editImageFocused from '../assets/edit-focused.png';
+import addImage from '../assets/add.png';
 
-import editImage from "../assets/edit.png";
-import editImageFocused from "../assets/edit-focused.png";
-import addImage from "../assets/add.png";
-
-import expandIconImage from "../assets/expand.png";
+import expandIconImage from '../assets/expand.png';
 export const expandIcon = expandIconImage;
 
-import logoInnerImage from "../assets/logo_inner.png";
+import logoInnerImage from '../assets/logo_inner.png';
 export const logoInnerIcon = logoInnerImage;
 
 export const editIcon = editImage;
@@ -47,135 +46,152 @@ export const routesTabFocused = routesIconFocused;
 export const schedulesTabFocused = schedulesIconFocused;
 export const balanceTabFocused = balanceIconFocused;
 
-export const MAIN_PRIMARY_COLOUR = "#3A3957";
+export const MAIN_PRIMARY_COLOUR = '#3A3957';
 export const ScreenHeadingStyles = {
-  fontSize: 54,
-  fontFamily: "WorkSans_500Medium",
-  color: MAIN_PRIMARY_COLOUR,
-  marginTop: 84,
+	fontSize: 54,
+	fontFamily: 'WorkSans_500Medium',
+	color: MAIN_PRIMARY_COLOUR,
+	marginTop: 84
 };
 
-export const Locations = ["Bella Vista Station", "QVB", "UNSW"];
+export const Locations = [ 'Bella Vista Station', 'QVB', 'UNSW' ];
 
 export const MenuBar = [
-  {
-    name: "HOME",
-    icon: homeIcon,
-  },
-  {
-    name: "ROUTES",
-    icon: routesIcon,
-  },
-  {
-    name: "SCHEDULES",
-    icon: schedulesIcon,
-  },
-  {
-    name: "BALANCE",
-    icon: balanceIcon,
-  },
+	{
+		name: 'HOME',
+		icon: homeIcon
+	},
+	{
+		name: 'ROUTES',
+		icon: routesIcon
+	},
+	{
+		name: 'SCHEDULES',
+		icon: schedulesIcon
+	},
+	{
+		name: 'BALANCE',
+		icon: balanceIcon
+	}
 ];
 
 export const UserTrips = [];
 
 export const DefinedTrips = [
-  {
-    startStop: "Bella Vista Station",
-    endStop: "UNSW",
-    nextTripTime: "5 MIN",
-    duration: "~ 1h 20m",
-    cost: "$6.99",
-    id: 0,
-    legs: [
-      {
-        image: busIcon,
-        desc: "45 Min",
-      },
-      {
-        image: walkIcon,
-        desc: "700m",
-      },
-      {
-        image: lightrailIcon,
-        desc: "15 Min",
-      },
-      {
-        image: walkIcon,
-        desc: "700m",
-      },
-    ],
-  },
-  {
-    startStop: "Bella Vista Station",
-    endStop: "UNSW",
-    nextTripTime: "7 MIN",
-    duration: "~ 1h 8m",
-    cost: "$5.99",
-    id: 1,
-    legs: [
-      {
-        image: trainIcon,
-        desc: "18 Min",
-      },
-      {
-        image: trainIcon,
-        desc: "35 Min",
-      },
-      {
-        image: walkIcon,
-        desc: "600m",
-      },
-      {
-        image: busIcon,
-        desc: "15 Min",
-      },
-    ],
-  },
-  {
-    startStop: "Kellyville Station",
-    endStop: "Macquarie Library",
-    nextTripTime: "9 MIN",
-    duration: "~ 33m",
-    cost: "$3.99",
-    id: 2,
-    legs: [
-      {
-        image: trainIcon,
-        desc: "22 Min",
-      },
-      {
-        image: walkIcon,
-        desc: "700m",
-      },
-    ],
-  },
-  {
-    startStop: "Bella Vista Station",
-    endStop: "QVB",
-    nextTripTime: "6 MIN",
-    duration: "~ 45m",
-    cost: "$4.99",
-    id: 3,
-    legs: [
-      {
-        image: busIcon,
-        desc: "45 Min",
-      },
-    ],
-  },
+	{
+		startStop: 'Bella Vista Station',
+		endStop: 'UNSW',
+		nextTripTime: '5 MIN',
+		duration: '~ 1h 20m',
+		cost: '$6.99',
+		id: 0,
+		legs: [
+			{
+				image: busIcon,
+				desc: '45 Min'
+			},
+			{
+				image: walkIcon,
+				desc: '700m'
+			},
+			{
+				image: lightrailIcon,
+				desc: '15 Min'
+			},
+			{
+				image: walkIcon,
+				desc: '700m'
+			}
+		]
+	},
+	{
+		startStop: 'Bella Vista Station',
+		endStop: 'UNSW',
+		nextTripTime: '7 MIN',
+		duration: '~ 1h 8m',
+		cost: '$5.99',
+		id: 1,
+		legs: [
+			{
+				image: trainIcon,
+				desc: '18 Min'
+			},
+			{
+				image: trainIcon,
+				desc: '35 Min'
+			},
+			{
+				image: walkIcon,
+				desc: '600m'
+			},
+			{
+				image: busIcon,
+				desc: '15 Min'
+			}
+		]
+	},
+	{
+		startStop: 'Kellyville Station',
+		endStop: 'Macquarie Library',
+		nextTripTime: '9 MIN',
+		duration: '~ 33m',
+		cost: '$3.99',
+		id: 2,
+		legs: [
+			{
+				image: trainIcon,
+				desc: '22 Min'
+			},
+			{
+				image: walkIcon,
+				desc: '700m'
+			}
+		]
+	},
+	{
+		startStop: 'Bella Vista Station',
+		endStop: 'QVB',
+		nextTripTime: '6 MIN',
+		duration: '~ 45m',
+		cost: '$4.99',
+		id: 3,
+		legs: [
+			{
+				image: busIcon,
+				desc: '45 Min'
+			}
+		]
+	}
+];
+
+export const OpalDetails = [
+	{
+		name: 'Bill',
+		type: 'Student',
+		OpalCardNum: '1234123412341234',
+		OpalCardPass: '1234',
+		OpalCurrBalance: 5
+	},
+	{
+		name: 'Jane',
+		type: 'Adult',
+		OpalCardNum: '4321432143214321',
+		OpalCardPass: '4321',
+		OpalCurrBalance: 5
+	}
 ];
 
 export const AllTripTimes = [
-  {
-    tripId: 0,
-    timesInterval: 8,
-    status: "ON TIME",
-    startStopExtended: "Bella Vista Station - Platform 2",
-    endStopExtended: "UNSW",
-    startHour: 12,
-    startMinute: 5,
-    totalMinutes: 45,
-  },
+	{
+		tripId: 0,
+		timesInterval: 8,
+		status: 'ON TIME',
+		startStopExtended: 'Bella Vista Station - Platform 2',
+		endStopExtended: 'UNSW',
+		startHour: 12,
+		startMinute: 5,
+		totalMinutes: 45
+	}
 ];
 
 // THIS IS A TEMPORARY CONSTANT, WILL BE DELETED ONCE REMINDERS ARE IMPLEMENTED.
--- app/screens/balance/BalanceScreen.js
+++ app/screens/balance/BalanceScreen.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, SafeAreaView, Text, TextInput, TouchableOpacity, Image } from 'react-native';
-import { MAIN_PRIMARY_COLOUR, ScreenHeadingStyles } from '../../constants';
+import { MAIN_PRIMARY_COLOUR, ScreenHeadingStyles, OpalDetails } from '../../constants';
 import { useNavigation } from '@react-navigation/native';
 import opalIcon from '../../assets/opal.png';
 
 export function BalanceScreen({ currentUser }) {
 	const navigation = useNavigation();
-
+	const [ opalCardNum, setOpalCardNum ] = useState('');
+	const [ opalCardPass, setOpalCardPass ] = useState('');
+	const [ userMatched, setUserMatched ] = useState(false);
 	return (
 		<SafeAreaView style={{ flex: 1 }}>
 			<View
@@ -29,76 +31,133 @@ export function BalanceScreen({ currentUser }) {
 				>
 					View and manage your Opal Balance.
 				</Text>
-				<Text
-					style={{
-						fontFamily: 'WorkSans_700Bold',
-						marginTop: 24,
-						alignSelf: 'flex-start',
-						color: MAIN_PRIMARY_COLOUR
-					}}
-				>
-					OPAL BALANCE
-				</Text>
-				<OpalCardInput placeholder="Opal Card Number..." />
-				<OpalCardInput placeholder="Opal Security Code or Password..." />
-
-				<View
-					style={{
-						flexDirection: 'row',
-						alignItems: 'center',
-						marginTop: 24,
-						paddingHorizontal: 24,
-						justifyContent: 'space-between',
-						width: '100%'
-					}}
-				>
-					<TouchableOpacity>
-						<Text
-							style={{
-								color: MAIN_PRIMARY_COLOUR,
-								fontFamily: 'WorkSans_400Regular',
-								fontSize: 10,
-								textDecorationLine: 'underline'
-							}}
-						>
-							Forgot username/password
-						</Text>
-					</TouchableOpacity>
-					<TouchableOpacity
-						style={{
-							flexDirection: 'row',
-							backgroundColor: MAIN_PRIMARY_COLOUR,
-							borderRadius: 24,
-							paddingHorizontal: 12,
-							paddingVertical: 8
-						}}
-					>
-						<Image
-							source={opalIcon}
-							style={{
-								width: 30,
-								resizeMode: 'contain',
-								marginRight: 2
-							}}
-						/>
-						<Text
-							style={{
-								color: 'white',
-								fontFamily: 'WorkSans_700Bold',
-								marginTop: -1,
-								fontSize: 10
-							}}
-						>
-							SIGN IN
-						</Text>
-					</TouchableOpacity>
-				</View>
+				{!userMatched ? (
+					<OpalSignIn
+						opalCardNum={opalCardNum}
+						opalCardPass={opalCardPass}
+						setOpalCardNum={setOpalCardNum}
+						setOpalCardPass={setOpalCardPass}
+						setUserMatched={setUserMatched}
+					/>
+				) : (
+					<DisplayOpalBalance userMatched={userMatched} setUserMatched={setUserMatched} />
+				)}
 			</View>
 		</SafeAreaView>
 	);
 }
 
-function OpalCardInput({ placeholder }) {
+function DisplayOpalBalance({ userMatched, setUserMatched }) {
+	console.log(userMatched);
+	return (
+		<View>
+			<Text
+				style={{
+					fontFamily: 'WorkSans_700Bold',
+					marginTop: 24,
+					alignSelf: 'flex-start',
+					color: MAIN_PRIMARY_COLOUR
+				}}
+			>
+				OPAL BALANCE - {userMatched['type']}, {userMatched['name']}
+			</Text>
+		</View>
+	);
+}
+
+function OpalSignIn({ opalCardNum, opalCardPass, setOpalCardNum, setOpalCardPass, setUserMatched }) {
+	return (
+		<View>
+			<Text
+				style={{
+					fontFamily: 'WorkSans_700Bold',
+					marginTop: 24,
+					alignSelf: 'flex-start',
+					color: MAIN_PRIMARY_COLOUR
+				}}
+			>
+				OPAL BALANCE
+			</Text>
+			<OpalCardInput placeholder="Opal Card Number..." text={opalCardNum} setText={setOpalCardNum} />
+			<OpalCardInput
+				placeholder="Opal Security Code or Password..."
+				text={opalCardPass}
+				setText={setOpalCardPass}
+			/>
+			<View
+				style={{
+					flexDirection: 'row',
+					alignItems: 'center',
+					marginTop: 24,
+					paddingHorizontal: 24,
+					justifyContent: 'space-between',
+					width: '100%'
+				}}
+			>
+				<TouchableOpacity>
+					<Text
+						style={{
+							color: MAIN_PRIMARY_COLOUR,
+							fontFamily: 'WorkSans_400Regular',
+							fontSize: 10,
+							textDecorationLine: 'underline'
+						}}
+					>
+						Forgot username/password
+					</Text>
+				</TouchableOpacity>
+				<TouchableOpacity
+					style={{
+						flexDirection: 'row',
+						backgroundColor: MAIN_PRIMARY_COLOUR,
+						borderRadius: 24,
+						paddingHorizontal: 12,
+						paddingVertical: 8
+					}}
+					onPress={() => {
+						console.log(opalCardNum);
+						console.log(opalCardPass);
+						console.log(OpalDetails);
+						const match = OpalDetails.filter((userDetails) => {
+							if (
+								userDetails['OpalCardNum'] === opalCardNum &&
+								userDetails['OpalCardPass'] === opalCardPass
+							) {
+								return userDetails;
+							}
+						});
+						if (match.length > 0) {
+							setUserMatched(match[0]);
+						} else {
+							// todo error message - invalid user/password.
+						}
+					}}
+				>
+					<Image
+						source={opalIcon}
+						style={{
+							width: 30,
+							resizeMode: 'contain',
+							marginRight: 2
+						}}
+					/>
+					<Text
+						style={{
+							color: 'white',
+							fontFamily: 'WorkSans_700Bold',
+							marginTop: -1,
+							fontSize: 10
+						}}
+					>
+						SIGN IN
+					</Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
+}
+
+function OpalCardInput({ placeholder, text, setText }) {
 	return (
 		<TextInput
 			style={{
@@ -112,6 +171,10 @@ function OpalCardInput({ placeholder }) {
 				color: MAIN_PRIMARY_COLOUR,
 				marginTop: 8
 			}}
+			onChangeText={(input) => {
+				setText(input);
+			}}
+			value={text}
 			placeholder={placeholder}
 		/>
 	);
