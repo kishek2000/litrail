84f39a8675f588452aac30f17bc8caf0360f24c2
TrollTown <williamyinanshen2009@gmail.com>
2020-08-14 01:04:13 +1000

    fix (expanded trip details): forgot to commit these in previous commit

--- app/components/tripdetailsexpanded/StopSequenceContainer.js
+++ app/components/tripdetailsexpanded/StopSequenceContainer.js
@@ -1,41 +1,26 @@
 import React from "react";
 import { View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-import { ExpandedTimeTile } from "./ExpandedTimeTile";
+import { ExpandedTimeTile } from "./TripDetailsUnexpandedStart";
 
 export function StopSequenceContainer({ expanded, stop_sequence }) {
   if (expanded) {
     return (
-      <View
-        style={{
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          justifyContent: "center",
           marginLeft: 20,
         }}
+        style={{
+          marginTop: 5,
+          marginBottom: 5,
+        }}
       >
-        <ScrollView
-          style={{
-            height: "72%",
-          }}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{
-            flexGrow: 1,
-            justifyContent: "center",
-          }}
-        >
-          <View
-            style={{
-              height: 10,
-            }}
-          ></View>
-          {stop_sequence.map((data, i) => (
-            <ExpandedTimeTile timeInfo={data}></ExpandedTimeTile>
-          ))}
-          <View
-            style={{
-              height: 5,
-            }}
-          ></View>
-        </ScrollView>
-      </View>
+        {stop_sequence.map((data, i) => (
+          <ExpandedTimeTile timeInfo={data}></ExpandedTimeTile>
+        ))}
+      </ScrollView>
     );
   }
   return <View></View>;
--- app/screens/routes/TripDetailsExpanded.js
+++ app/screens/routes/TripDetailsExpanded.js
@@ -1,71 +1,49 @@
 import React, { useState } from "react";
 import { View, SafeAreaView, Text } from "react-native";
-import { MAIN_PRIMARY_COLOUR } from "../../constants";
+import { MAIN_PRIMARY_COLOUR, ScreenHeadingStyles } from "../../constants";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 import { useRoute } from "@react-navigation/native";
 
 import { TripDetailsExpandedBody } from "../../components/tripdetailsexpanded/TripDetailsExpandedBody";
 import { hintTextStyles } from "../../components/tripdetails/TripDetailStyles";
+import { GoBackButton } from "./GoBackButton";
 
 export function TripDetailsExpanded({ navigation }) {
   const route = useRoute();
   const trip_id = route.params.trip_id;
-  console.log("inside expanded: trip_id: " + trip_id);
   const [expanded, setExpanded] = useState(false); // Controls the hint text
 
   return (
-    <SafeAreaView>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center",
+        position: "relative",
+      }}
+    >
       <View
         style={{
           flexDirection: "row",
           marginTop: "20%",
+          width: "100%",
           justifyContent: "center",
+          justifyContent: "space-between",
+          position: "absolute",
+          alignItems: "center",
+          marginTop: 106,
+          paddingLeft: 36,
         }}
       >
-        <View
-          style={{
-            flex: 2,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <TouchableOpacity
-            style={{
-              width: 60,
-              height: 40,
-              top: 2,
-              // backgroundColor: "pink",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            onPress={() => {
-              navigation.navigate("TripDetails", { trip_id: trip_id });
-            }}
-          >
-            <Text
-              style={{
-                fontFamily: "WorkSans_400Regular",
-                fontSize: 18,
-                color: MAIN_PRIMARY_COLOUR,
-              }}
-            >
-              BACK
-            </Text>
-          </TouchableOpacity>
+        <View>
+          <GoBackButton navigation={navigation} />
         </View>
-
-        <Text
-          style={{
-            fontSize: 40,
-            fontFamily: "WorkSans_500Medium",
-            color: MAIN_PRIMARY_COLOUR,
-            flex: 6,
-          }}
-        >
-          Trip Stops
-        </Text>
       </View>
+      <View>
+        <Text style={ScreenHeadingStyles}>Trip Stops</Text>
+      </View>
+
       <View
         style={{
           top: 5,
